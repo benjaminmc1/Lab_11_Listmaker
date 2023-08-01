27d1d57affc7b252ffabbc82b508073ad2e7b67c
Benjamin <bmckee566@gmail.com>
2023-08-01 15:34:49 -0400

    Actual project finish

--- src/Main.java
+++ src/Main.java
@@ -15,19 +15,37 @@ public class Main {
 
             if(query.equals("A") || query.equals("a") || query.equals("P") || query.equals("p") || query.equals("D") || query.equals("d") || query.equals("Q") || query.equals("q")) {
                 if(query.equals("A") || query.equals("a")) {
-                    userIn.add(SafeInput.getNonZeroLenString(in, "What would you like to add?"));
+                    addToArray(userIn, SafeInput.getNonZeroLenString(in, "What would you like to add?"));
                 } else if (query.equals("D") || query.equals("d")) {
                     displayNum(userIn);
-                    userIn.remove((SafeInput.getRangedInt(in, "Which part of the array would you like to remove?", 1, userIn.size())) - 1);
+                    deleteFromArray(userIn, (SafeInput.getRangedInt(in, "Which part of the array would you like to remove?", 1, userIn.size())) - 1);
                 } else if (query.equals("P") || query.equals("p")) {
                     display(userIn);
                 } else if (query.equals("Q") || query.equals("q")) {
-                    YorN = SafeInput.getYNConfirm(in, "Are you sure you would like to quit?");
+                    quitArray(userIn, YorN, in, "Are you sure you would like to quit?");
                 }
             } else{
                 System.out.println("Invalid entry. Try again.");
             }
         }while(!YorN);
+    }
+
+    private static void addToArray(ArrayList<String> array, String prompt) {
+        array.add(prompt);
+    }
+
+    private static void deleteFromArray(ArrayList<String> array, int i) {
+        displayNum(array);
+        array.remove(i);
+    }
+
+    private static boolean quitArray(ArrayList<String> array, boolean quit, Scanner pipe, String prompt) {
+        quit = SafeInput.getYNConfirm(pipe, prompt);
+        if(quit) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private static String display(ArrayList<String> array) {
